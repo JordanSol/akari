48c64bc3efc932e913aa56c8b4e53805a8a2405e
JordanSol <jordansolfreeman@outlook.com>
2022-02-10 12:30:48 -0800

    trying img

--- components/Background.js
+++ components/Background.js
@@ -1,11 +1,23 @@
 import { useParallax } from 'react-scroll-parallax'
+import { motion } from 'framer-motion'
 
-const Background = ({children, speed = 10, z = 0}) => {
+const Background = ({children, speed = 10, initSize, dur, del }) => {
     const { ref } = useParallax({ speed: speed})
     return (
-        <div ref={ref} className={`w-screen h-screen absolute top-0 left-0 z-${z}`}>
+        <motion.div ref={ref} className={`w-screen h-screen absolute top-0 left-0`}
+            initial={{
+                scale: initSize,
+            }}
+            animate={{
+                scale: 1,
+                transition: {
+                    duration: dur,
+                    delay: del
+                }
+            }}
+        >
             {children}
-        </div>
+        </motion.div>
     )
 }
 
