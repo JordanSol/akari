1f3685f99172ecf79cc21c1193545fc29055270a
JordanSol <jordansolfreeman@outlook.com>
2022-02-10 01:15:45 -0800

    fixed fonts

--- pages/_app.js
+++ pages/_app.js
@@ -9,9 +9,6 @@ function MyApp({ Component, pageProps, router }) {
       <Head>
         <title>Akari NFT</title>
         <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32"/>
-        <link rel="stylesheet" href="https://use.typekit.net/vrc4mmd.css"/>
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
       </Head>
         <Layout>
           <motion.div key={router.route} initial='pageInitial' animate='pageAnimate' variants={{
