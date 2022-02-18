d27be4ef4330e9d6374e7b5836469b27e4a4c450
Jordan <jordansolfreeman@outlook.com>
2022-02-18 15:59:35 -0800

    new carousel images

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b84724a5fb211971"; }
+/******/ 		__webpack_require__.h = function() { return "1a1cec1efcf6e2a7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/carouselmages.js
+++ components/carouselmages.js
@@ -3,7 +3,9 @@ import aime2 from '../assets/aime2.png'
 import blueoni from '../assets/blueoni.png'
 import redoni from '../assets/redoni.png'
 import trueoni from '../assets/trueoni.png'
+import fireoni from '../assets/FireOni.png'
+import kunai from '../assets/Kunai.png'
 
-const images = [wukong, blueoni, trueoni, redoni, aime2]
+const images = [kunai, wukong, blueoni, fireoni, trueoni, redoni, aime2]
 
 export default images
